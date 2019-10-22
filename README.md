6d901437a3595fe8e0afee38873388eef9984c28
Benoit Suttor <bsuttor@imio.be>
2019-10-22 14:01:07 +0200

    Initial commit from gatsby: (https://github.com/collective/gatsby-starter-plone.git)

---
+++ README.md
@@ -0,0 +1,28 @@
+# gatsby-starter-plone
+
+Gatsby starter for Plone
+
+## Install
+
+Make sure you have Gatsby CLI installed globally:
+
+```sh
+npm install --global gatsby-cli
+```
+
+Use this starter from Gatsby CLI:
+
+```sh
+gatsby new <your-project-name> https://github.com/collective/gatsby-starter-plone
+```
+
+Then you can run it by:
+
+```sh
+cd <your-project-name>
+gatsby develop
+```
+
+## Deploy
+
+[![Deploy to Netlify](https://www.netlify.com/img/deploy/button.svg)](https://app.netlify.com/start/deploy?repository=https://github.com/gatsbyjs/gatsby-starter-default)
---
+++ src/pages/index.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+
+import Layout from '../components/Layout';
+import Document from '../components/Document';
+import Folder from '../components/Folder';
+
+const nodes = query => (query ? query['edges'] : []).map(edge => edge.node);
+
+const IndexPage = ({ data }) => (
+  <Layout>
+    <Document
+      data={data.ploneDocument}
+      images={nodes(data['allPloneImage'])}
+      files={nodes(data['allPloneFile'])}
+    />
+    <hr style={{ background: '#e8eef2' }} />
+    <Folder
+      data={data.ploneSite}
+      title="Contents"
+      images={nodes(data['allSitePloneImage'])}
+      files={nodes(data['allSitePloneFile'])}
+    />
+  </Layout>
+);
+
+export default IndexPage;
+
+export const query = graphql`
+  query IndexPageQuery {
+    ploneDocument(_path: { in: ["/frontpage/", "/front-page/"] }) {
+      ...Document
+    }
+    allPloneFile(
+      filter: { _backlinks: { in: ["/frontpage/", "/front-page/"] } }
+    ) {
+      edges {
+        node {
+          ...File
+        }
+      }
+    }
+    allPloneImage(
+      filter: { _backlinks: { in: ["/frontpage/", "/front-page/"] } }
+    ) {
+      edges {
+        node {
+          ...Image
+        }
+      }
+    }
+    ploneSite(_path: { eq: "/" }) {
+      ...Site
+    }
+    allSitePloneFile: allPloneFile(filter: { _backlinks: { eq: "/" } }) {
+      edges {
+        node {
+          ...File
+        }
+      }
+    }
+    allSitePloneImage: allPloneImage(filter: { _backlinks: { eq: "/" } }) {
+      edges {
+        node {
+          ...Image
+        }
+      }
+    }
+  }
+`;
